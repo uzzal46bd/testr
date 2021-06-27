fac09154cee428fbfe6dcad526d54367dad939d8
Mohammad Amin <uzzal46bd@yahoo.com>
2021-06-27 22:10:44 +0200

    merge from main

--- server.js
+++ server.js
@@ -12,11 +12,8 @@ var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
 
-<<<<<<< HEAD
 //Change from feature uzz46
 //Change1 from feature uzz46
 //Change from feature ma46
 //Change from feature2 ma46
-=======
 //Change from main ma46
->>>>>>> main
